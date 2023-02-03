1855667adb38fcbd96524da104f0cf1d36a1e49d
Marten09 <leshadyuckarev631@gmail.com>
2023-02-03 19:08:59 +0300

    new class

---
+++ src/com/startjava/lesson_4/guess/Player.java
@@ -0,0 +1,48 @@
+package com.startjava.lesson_4.guess;
+
+import java.util.Arrays;
+
+public class Player {
+    public static final int MAX_TRY_COUNT = 10;
+    private int[] enteredNumbers;
+    private String name;
+    private int tryCount;
+
+    public Player(String name) {
+        this.name = name;
+        enteredNumbers = new int[MAX_TRY_COUNT];
+    }
+
+    public int[] getEnteredNumbers() {
+        return (tryCount > 0 ? Arrays.copyOf(enteredNumbers, tryCount) : new int[0]);
+    }
+
+    public int getLastNumber() {
+        return tryCount > 0 ? enteredNumbers[tryCount - 1] : 0;
+    }
+
+    public void addNumber(int number) {
+        if (noMoreTries()) {
+            return;
+        }
+        enteredNumbers[tryCount] = number;
+        tryCount++;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getTryCount() {
+        return tryCount;
+    }
+
+    public boolean noMoreTries() {
+        return tryCount == MAX_TRY_COUNT;
+    }
+
+    public void reset() {
+        Arrays.fill(enteredNumbers, 0, tryCount, 0);
+        tryCount = 0;
+    }
+}
